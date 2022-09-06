bffdf9801c074a374e70cc78506bba3c4499fdab
ChaySittichok <sittichok.3345@mail.kmutt.ac.th>
2022-09-07 02:15:17 +0700

    summit

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,16 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  if((word == null || undefined )){
+    return undefined
+  }
+  else if(word.includes(startWord)){
+    return word
+  }
+  else {
+    let arr1
+    return arr1 = startWord.concat(word)
+  }
+  
 }
 module.exports = fillStartWord
